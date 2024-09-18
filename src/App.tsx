71b45fa76d8c72f885fe6cfacbd8701a6990d04d
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-09-18 13:58:37 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button.tsx";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Button variant="default">Hello</Button>
+      </div>
+    </>
+  );
+
+}
